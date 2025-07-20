05f7dc9d97f37899124ccbf3d3470a4291967f18
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-07-20 10:45:06 -0300

    feat: conversão dados DTO para JPA

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos") //URL Requisitada da API
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     //Requisição via POST
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) { //o parametro é o "corpo da requisição"
-        System.out.println(dados);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) { //dto
+        repository.save(new Medico(dados)); //Conversão de DTO para tipo Medico
     }
 }
--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,4 +1,4 @@
 package med.voll.api.endereco;
 
-public record DadosEndereco(String cidade, String uf, String cep, String bairro, String rua, String numero, String complemento) {
+public record DadosEndereco(String logradouro, String cidade, String uf, String cep, String bairro, String rua, String numero, String complemento) {
 }
